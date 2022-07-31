69d4a04d73fa446b14873cf7f4488fb2aed329cc
UTSHPE <42721471+UTSHPE@users.noreply.github.com>
2022-07-31 15:54:20 -0600

    Added About us page and pillars page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,31 @@ $(document).ready( function() {
         this.requestFullscreen();
     });
 });
+
+function reveal() {
+    var reveal_l = document.querySelectorAll(".reveal-left");
+    var reveal_r = document.querySelectorAll(".reveal-right");
+    for (var i = 0; i < reveal_l.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveal_l[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveal_l[i].classList.add("active");
+      } else {
+        reveal_l[i].classList.remove("active");
+      }
+    }
+
+    for (var i = 0; i < reveal_r.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveal_r[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveal_r[i].classList.add("active");
+      } else {
+        reveal_r[i].classList.remove("active");
+      }
+    }
+}
+
+window.addEventListener("scroll", reveal);
